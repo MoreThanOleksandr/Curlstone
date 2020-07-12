21b37b5f4bb4fff3f13de36522a212a4e79309cb
oleksadnr kostyuk <morethanoleksandr@gmail.com>
2020-07-12 19:22:55 +0200

    aded index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,33 @@
-$(function(){
-    
+$(function () {
+
+    $('.main-slider__inner').slick({
+        arrows: false,
+        infinite: true,
+        asNavFor: '.dots-slider__inner'
+    });
+    $('.dots-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        
+        infinite: true,
+        arrows: false,
+        asNavFor: '.main-slider__inner'
+    });
+    $('.works-slider').slick({
+        arrows: false,
+        centerMode: true,
+        infinite: true,
+        variableWidth: true
+    });
+    $('.properties-slider').slick({
+        arrows: false,
+        centerMode: true,
+        infinite: true,
+        variableWidth: true
+    });
+
+    $('select').styler();
+
 
 
 })
--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ gulp.task('css',function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css'
     ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -45,6 +46,7 @@ gulp.task('script',function(){
 gulp.task('js',function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
